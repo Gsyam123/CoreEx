2d6eeab0907d90fc96465a80fab8af4338b275b9
unknown <gsyamkumar102@gmail.com>
2022-01-28 10:46:14 +0530

    sai

--- CoreEx/src/github/Ex1.java
+++ CoreEx/src/github/Ex1.java
@@ -1,9 +1,10 @@
 package github;
 
 public class Ex1 {
-public static void main(String[] args) {
-	System.out.println("welcome to Github");
-	
-}
+	public static void main(String[] args) {
+		System.out.println("welcome to Github");
+		System.out.println("Sai krishna Singh");
+
+	}
 
 }
